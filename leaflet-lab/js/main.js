58dce1eafc5bd9ab128380e500264f4fb963d0cb
melanie-n-k <45474048+melanie-n-k@users.noreply.github.com>
2019-03-05 11:08:26 -0600

    sequence
    finally got sequence to work by reorganizing all functions to separate them out a bit

--- leaflet-lab/js/main.js
+++ leaflet-lab/js/main.js
@@ -3,129 +3,162 @@
 
 /* Map of GeoJSON data from Wisconsin Public Library data */
 
-//function to instantiate the Leaflet map
-function createMap(){
-    //create the map and center it on WI
-    var map = L.map('map', {
-        center: [45, -88],
-        zoom: 7
-    });
-
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-    //call getData function
-    getData(map);
-};
-function createSequenceControls(map){
+//create slider and skip buttons
+function createSequenceControls(map,attributes){
     //create range input element (slider)
     $('#panel').append('<input class="range-slider" type="range">');
-    //listener for slider
-    $('.range-slider').on('input', function(){
-      var index = $(this).val();
-      //console.log(index);
+	//set slider attributes
+	$('.range-slider').attr({
+		max: 8,
+		min: 0,
+		value: 0,
+		step: 1
+	})
+	//add skip buttons
+  $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+  $('#panel').append('<button class="skip" id="forward">Skip</button>');
+  //replace button content with images
+  $('#reverse').html('<img src="img/reverse.png">');
+  $('#forward').html('<img src="img/forward.png">');
+  //listener for slider
+  $('.range-slider').on('input', function(){
+    var index = $(this).val();
+    //console.log(index);
+    //update symbols when slider is moved
+    updatePropSymbols(map, attributes[index]);
 });
-    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
-    $('#panel').append('<button class="skip" id="forward">Skip</button>');
-    //Below Example 3.5...replace button content with images
-    $('#reverse').html('<img src="img/reverse.png">');
-    $('#forward').html('<img src="img/forward.png">');
-    //click listener for buttons
     $('.skip').click(function(){
-      //get the old index value
-      var index = $('.range-slider').val();
-      //increment or decrement depending on button clicked
-      if ($(this).attr('id') == 'forward'){
-          index++;
-          //if past the last attribute, wrap around to first attribute
-          index = index > 6 ? 0 : index;
-      } else if ($(this).attr('id') == 'reverse'){
-          index--;
-          //if past the first attribute, wrap around to last attribute
-          index = index < 0 ? 6 : index;
-      };
-      //update slider
-      $('.range-slider').val(index);
-});
-    //set slider attributes
-    $('.range-slider').attr({
-        max: 6,
-        min: 0,
-        value: 0,
-        step: 1
+        //get old index value
+        var index = $('.range-slider').val();
+        //increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //if past the last attribute, wrap around to first attribute
+            index = index > 8 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 8 : index;
+        };
+        //update slider
+        $('.range-slider').val(index);
+		//update symbols when buttons are clicked
+        updatePropSymbols(map, attributes[index]);
     });
-  };
-//build an attributes array from the data
+};
+
+//connect proportional symbols to cycling attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+            //add city to popup content string
+            var popupContent = "<p><b>" + props.N + ": </b></p>";
+            //add formatted attribute to content string
+            var year = attribute.split("_")[2];
+            popupContent += "<p><b>" + props[attribute] + "</b> print books in " + year + "</p>";
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+	});
+};
+
+//create and style the proportional symbols
+function pointToLayer(feature, latlng, attributes){
+	var attribute = attributes[0];
+	var geojsonMarkerOptions = {
+		radius: 8, //initial radius before calculations
+		fillColor: "#FA7268",
+		color: "#FA7268",
+		weight: 1.5,
+		opacity: 1,
+		fillOpacity: 0.75
+	};
+	var attValue = Number(feature.properties[attribute]);
+	geojsonMarkerOptions.radius = calcPropRadius(attValue);
+	var layer = L.circleMarker(latlng, geojsonMarkerOptions);
+	//build popup content string
+	var popupContent = "<p><b>" + feature.properties.N + ": </b></p>";
+	//add formatted attribute to popup content
+	var year = attribute.split("_")[2];
+	popupContent += "<p><b>" + feature.properties[attribute] +"</b> print books in " + year + "</p>";
+	//bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+		offset: new L.point(0, -geojsonMarkerOptions.radius)
+	});
+	return layer
+};
+
+//create array to hold index values for attributes
 function processData(data){
     //empty array to hold attributes
-    var attArray = [];
+    var attributes = [];
     //properties of the first feature in the dataset
     var properties = data.features[0].properties;
     //push each attribute name into attributes array
     for (var attribute in properties){
         //only take attributes with population values
         if (attribute.indexOf("Print") > -1){
-            attArray.push(attribute);
+            attributes.push(attribute);
         };
     };
-    //check result
-    //console.log(attArray);
-    return attArray;
+    return attributes;
 };
 
-//function to retrieve the data and place it on the map
+//adjust the symbols for each data point to reflect its value using the calcPropRadius function results
+function createPropSymbols(data, map, attributes){
+	L.geoJson(data, {
+		pointToLayer: function(feature,latlng){
+			return pointToLayer(feature,latlng,attributes);
+		}
+	}).addTo(map);
+};
+
+//function to get that data
 function getData(map){
     //load the data
     $.ajax("data/Books.geojson", {
         dataType: "json",
         success: function(response){
-            var attArray = processData(response);
-            //console.log(attArray);
-            createSequenceControls(map, attArray);
-            //create a Leaflet GeoJSON layer and add it to the map
-            geojsonLayer = L.geoJson(response, {
-              //function to calculate radius size for symbols
-              pointToLayer: function(feature, latlng, attArray) {
-                var attArray = processData(response);
-                var attribute = attArray[0];
-                //console.log(attribute);
-                var attValue = Number(feature.properties[attribute]);
-                //console.log(calcPropRadius(attValue));
-                function calcPropRadius(attValue) {
-                  //scale factor to adjust symbol size evenly
-                  var scaleFactor = 0.02;
-                  //area based on attribute value and scale factor
-                  var area = attValue * scaleFactor;
-                  //radius calculated based on area
-                  var radius = Math.sqrt(area/Math.PI);
+			//create an attributes array
+            var attributes = processData(response);
+			createPropSymbols(response, map, attributes);
+			createSequenceControls(map,attributes);
+		}
+    });
+};
 
-                  return radius;
-                };
-                //use return to generate proportional symbols
-                      return new L.CircleMarker(latlng, {
-                        radius: calcPropRadius(attValue),
-                        fillColor: "#FA7268",
-                        color: "#FA7268",
-                        weight: 1.5,
-                        opacity: 1,
-                        fillOpacity: 0.75
-                      });
-                  },
-              //function to add popups to proportional symbols
-              onEachFeature: function(feature, layer) {
-                      var attArray = processData(response);
-                      var attribute = attArray[0];
-                      var year = attribute.split("_")[2];
-                      layer.bindPopup("<b>" + feature.properties.N + "<br>" + feature.properties.Print_Books_2017 + "</b>" + " print books in " + year);
-                    },
-             })
-        map.addLayer(geojsonLayer);
-          }
-      })
-   };
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 0.02;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+    return radius;
+};
 
+//function to create the map
+function createMap(){
+    //create the map
+    var map = L.map('map', {
+        center: [45, -88],
+        zoom: 7
+    });
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+    //call getData function to add data to the map
+    getData(map);
+};
 
-
+//creates the entire map once the page is loaded
 $(document).ready(createMap);
